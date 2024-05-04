6405d07747f4c3d5bf26fd27bc08627452ce45d1
Salman0348 <salamankhan0348@gmail.com>
2024-05-04 21:33:31 +0500

    developed header and footer

---
+++ script.js
@@ -0,0 +1,9 @@
+// (function(){
+//     Array.from(document.querySelectorAll('.menuItem')).forEach(menuItem => {
+//         menuItem.addEventListener('click',function(){
+//             document.querySelector('.activeItem').classList.remove('activeItem');
+//             this.classList.add('activeItem');
+            
+//         })
+//     })
+// })();
